a07c0a8e4ba6ecc0ea5d5be346e3e2e74e503789
RicardoShepstone <richardignacio888@gmail.com>
2026-07-17 13:41:41 +0200

    ya funciona el entrenamiento, no parece quie de buenos resultados, esperamos a que acabe

--- trainer/test.py
+++ trainer/test.py
@@ -31,6 +31,8 @@ def validation(model, criterion, evaluation_loader, converter, opt, device):
         text_for_pred = torch.LongTensor(batch_size, opt.batch_max_length + 1).fill_(0).to(device)
 
         text_for_loss, length_for_loss = converter.encode(labels, batch_max_length=opt.batch_max_length)
+        text_for_loss = text_for_loss.to(device)
+        length_for_loss = length_for_loss.to(device)
         
         start_time = time.time()
         if 'CTC' in opt.Prediction:
@@ -38,7 +40,7 @@ def validation(model, criterion, evaluation_loader, converter, opt, device):
             forward_time = time.time() - start_time
 
             # Calculate evaluation loss for CTC decoder.
-            preds_size = torch.IntTensor([preds.size(1)] * batch_size)
+            preds_size = torch.IntTensor([preds.size(1)] * batch_size).to(device)
             # permute 'preds' to use CTCloss format
             cost = criterion(preds.log_softmax(2).permute(1, 0, 2), text_for_loss, preds_size, length_for_loss)
 
